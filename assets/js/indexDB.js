22057af82bb75fe8cb90b1e994e26427e9ceb5b7
Mekides <se.mekides.bishaw@gmail.com>
2021-02-25 15:04:43 -1200

    Updating indexDB.js file inorder to the database track the informatio from the user input

--- assets/js/indexDB.js
+++ assets/js/indexDB.js
@@ -1,5 +1,10 @@
 import exptrcker from './module.js';
 
 export let db = exptrcker("ExpenseTracker", {
-    Users: "++id,UserName,Password,AccountNumber"
+    expense: `++id, userName, category, expense_type, amount, date, time`,
+    income: `++id, userName, category, income_type, amount, date, time`,
+    Users: `++id,UserName,Password,AccountNumber`,
+    group_expense: `++id, groupname, team_member, category, date, info`,
+    group_income: `++id, groupname, team_member, category, date, info`,
+    ikub_expense: `++id, ikubname, team_member, begin_date, interval, amount_per_round, punishment_fee, info`,
 });
